64f6949ca9d35d3089cb7c273d925f65625d15d4
Gloria Ngoya <glowngoya@gmail.com>
2022-03-09 08:56:08 +0300

    First Commit

---
+++ CallCenter.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Hive Office Suites </title>
+
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="img/Background.svg" /> <!--LOGO At the topp of the search bar -->
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/landing-page.css" rel="stylesheet">
+  
+  </head>
+  <body>
+      <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="mainNav">
+              <div class="container">
+                  <a class="navbar-brand" href="#page-top"><img src="img/Aushaan-logo.svg" alt="..." /></a><!--LOGO At the navigation bar -->
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                      Menu
+                      <i class="fas fa-bars ms-1"></i>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarResponsive">
+                      <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0" >
+                          <li class="nav-item"><a class="nav-link" href="LandingPage.php">HOME</a></li>
+                          <li class="nav-item"><a class="nav-link" href="Membership.php">MEMBERSHIP</a></li>
+                          <li class="nav-item"><a class="nav-link" href="ContactUs.php">CONTACT US</a></li>
+                          <li class="nav-item"><a class="nav-link" href="AboutUs.php">ABOUT US</a></li>
+                      </ul>
+                  </div>
+              </div>
+    </nav>        
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</header>
+
+
+<div ></div>
+
+
+</div>
+<!-- Masthead -->
+<header class="masthead text-white text-center">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <h1 class="mb-5">The Hive Call Center <br>  </h1>
+            <h2>Streamline all your customer communications with our outsourced Call Center Package.</h2>
+            <a><button class="btn btn-primary" type="button">Request for a Quote</button></a>
+          </div>
+          <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+            <form>
+              <div class="form-row">
+                <div class="col-xl-9 mx-auto">
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </header>
+    <section class="sp-head">
+  <div class="container">
+    <article class="sp-head__content"> <br><br>
+     <h1 style="font-size: 35px; text-align:center;" >Improve customer service levels by offering 24/7 support to customers
+     and reduce transaction costs without affecting service quality</h1>
+    </article>
+    <p style="text-align: center; color: black; font-size: 22px;" >
+    Dedicated and shared call center agents as per your requirement. 
+    </p>
+    </div>  
+    <!-- image showcase-->
+    <section class="showcase">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters">
+
+          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/telephone.jpg');max-width:100%;"></div>
+          <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h3 style="color: #233a77;text-align: left;" class="display-5 mb-0" >The Hive Call Center Package</h3><br>
+              <p class="lead mb-0" style="text-align: justify;font-weight:400;">  
+              Are you looking to improve customer service levels and offer 24/7 support for customers, <b> reduce the number of customer 
+              complaints and/or reduce transaction costs without affecting service quality?</b>
+ <br>
+ At the Hive we help streamline all your customer communications with our outsourced Call Center Package.
+<br> Choose a plan that is convenient for your business, whether to have a dedicated team or cost-effective shared call center agents. 
+<br> <b>Our services are flexible, you can also scale up or down to handle seasonal requests </b> as well as curate a
+ mixed team of shared and dedicated call center agents. <br><br>
+<h4>Check out the services Offered and <br> Talk to us today!</h4>
+</p> 
+
+        </div>
+        </div>
+        </div>  
+   <!-- Text Showcases -->
+   
+<section>
+        <div class="row no-gutters">
+        <div class="col-lg-6 p-6 order-lg-4 my-auto text-center">
+            <br/>
+            <br/>
+            <h2 class="display-5 mb-0" style="text-align:center"> Inbound Voice Support </h2>
+            <br/>
+            <p class="lead mb-0" style="text-align:justify">
+        <ul class="lead mb-0" style="text-align: justify;font-weight:400;">
+           <br> <li> <b> General Inquiries:</b> Give customers information when they need it most. 
+                Our friendly and helpful executives impress and assure your customers.</li>
+            <br> <li> <b>Billing Queries:</b> Don’t let payment related queries linger on. 
+                Let us help your customers understand their bill and explain charges.</li>
+            <br> <li> <b>Order Taking:</b> Keep sales moving along continuously without disruptions.
+                 We record orders accurately and communicate in a professional manner.</li>
+           <br> <li><b>Customer Issues:</b> Solve problems with immediacy before they blow up. 
+                Our executives address problems promptly and keep your customers satisfied.</li>
+            <br> <li><b>Product Information: </b>Make it easy for customers to understand your product. 
+                With quick access to information, your customers can buy or use your product proactively.</li>
+           </ul>
+        </p>       
+        </div>
+        <div class="col-lg-6 p-6 order-lg-4 my-auto text-center">
+            <br/>
+            <br/>
+            <br/>
+            <h2 class="display-5 mb-0" style="text-align:center"> Outbound Voice Support </h2>
+            <br/>
+            <ul class="lead mb-0" style="text-align: justify;font-weight:400;">
+               <br> <li> <b> Surveys:</b> Supplement your in-house market research with customer surveys to learn 
+                    about current sentiment, expectations and challenges.</li>
+               <br> <li><b>Telemarketing: </b>Promote products and present sales pitches to drive sales and update
+                     customers about the latest products, services and plans.</li>
+               <br> <li><b>Lead generation: </b>Generate business leads and grow your customer 
+                    list in a structured, consistent and continuous manner.</li>
+               <br> <li><b>Collections: </b>Outsource debt collection and pending payments to enjoy peace of mind knowing 
+                    that dedicated representatives are handling these sensitive communications proactively.</li>
+               <br> <li><b>Follow-ups:</b> Don’t ever forget to follow-up or delay an important moment in the prospect’s journey.</li>
+               <br> <li><b>Appointment Booking: </b>Lead potential customers’ through the sales lifecycle by initiating 
+                    contact and setting up meetings.</li>
+                
+            </ul>          
+            
+            </div>
+        <div class="col-lg-6 p-6 order-lg-4 my-auto text-center">
+            <br/>
+            <br/> <br>
+            <h2 class="display-5 mb-0" style="text-align:center"> Email Support  </h2>
+            <br/>
+            <p class="lead mb-0" style="text-align:justify">
+        <ul class="lead mb-0" style="text-align: justify;font-weight:400;">
+         <br>   <li> <b>Query Resolutions: </b> Show customers you care by being there when they need you most. 
+                 Our real-time database and deep understanding of your business enables us to address questions 
+                and manage feedback intelligently and efficiently.</li>
+           <br> <li> <b>Product Inquiries: </b> Move hot prospects rapidly along the sales cycle. 
+                    Reinforce the confidence of existing customers by giving them the information 
+                    they seek in the quickest possible time via email.</li>
+           <br> <li> <b>Order Taking and Fulfilment: </b> Deliver impeccable email service at a crucial juncture
+                 in the customer journey. This experience will determine whether or not customers
+                  keep coming to your product or service offerings.</li>
+           <br> <li><b>Payment Inquiries and Documentation: </b> We are fully invested in providing the
+            technical and procedural support your customers need, and your in-house team can count on.</li>
+           <br> <li><b>Product/Service Information Requests and Troubleshooting:  </b>We function as a knowledge 
+           support touchpoint, responding promptly to requests for further information, and problem-solving in a time-bound manner.</li>
+           </ul>
+        </p>       
+        </div>
+        <div class="col-lg-6 p-6 order-lg-4 my-auto text-center">
+            <br/>
+            <br/>
+            <br/>
+            <h2 class="display-5 mb-0" style="text-align:center"> Technical Support </h2>
+            <br/>
+            <ul class="lead mb-0" style="text-align: justify;font-weight:400;">
+            <br> <li><b>Help Desk:  </b>Connect your customers to our knowledgeable and dynamic help desk for all your business related
+                                     questions, support and troubleshooting.</li>
+               <br> <li><b>Remote Assistance: </b> Don’t let large geographical distances and time zone differences limit your
+                                 ability to deliver efficient customer support.</li>
+               <br> <li><b>Software Assistance:  </b>With a direct line to phone assistance, a dedicated team handling email 
+                                support or live website chats, your customers never have to feel overwhelmed with your software solution.</li>
+                                </ul> <br>
+             <h2 class="display-5 mb-0" style="text-align:center"> Live Chat Support  </h2>
+              <ul  class="lead mb-0" style="text-align: justify;font-weight:400;">
+               <br> <li>24/7 live chat services to respond to customer inquiries no matter the time of day</li>
+               <br> <li>Secure, private conversation channels to protect the privacy of your customers</li>
+               <br> <li>Experienced, friendly, and dedicated support representatives</li>
+            </ul>        
+        </div>
+        </div>
+        </div>
+        
+        </div>
+    </div>
+
+</div>
+<div class="row no-gutters">
+          <div class="col-lg-4 text-white showcase-img" style="background-image: url('img/advantage.jpg'); "></div>
+          <div class="col-lg-8 my-auto showcase-text">
+          <h3 style="color: #233a77;text-align: left;" class="display-5 mb-0" > The Hive Call Center Advantage</h3><br>
+          <p class="lead mb-0" style="text-align: justify;font-weight:500;">
+           
+                      <ul class="lead mb-0" style="text-align: justify;font-weight:400;">
+                        <li>Improved Productivity for small businesses looking to make a mark in their market, 
+                            our inbound call center provides low volume businesses with skilled agents trained 
+                            to manage increasing call volume as you grow your business.</li>
+                        <li>Increased Sales -  Our digital marketing support will come in handy to ensure 
+                            your current customers are satisfied, while increasing product sales as well.  </li>
+                        <li>Customer Focused – Our dedicated captive call center services allows you to enjoy 
+                            dedicated agent only handling your customer needs. </li>
+                        <li>Customer Satisfaction - Our inbound call center agent can gauge the customers' 
+                            pulse and understand what they want, need, and expect from the company.</li>
+                      </ul>
+                                            
+        </div>
+        </div>
+        </div>
+        <div class="row no-gutters">
+        <div class="col-lg-6 p-6 order-lg-4 my-auto text-center">
+            <br/>
+            <br/>
+            <h2 class="display-5 mb-0" style="text-align:center"> Call Center Outsourcing Process </h2>
+            <br/>
+            <p class="lead mb-0" style="text-align:justify">
+        <ul class="lead mb-0" style="text-align: justify;font-weight:400;">
+             <li>Planning </li>
+             <li> Documenting</li>
+             <li> Training</li>
+             <li>Intergration</li>
+             <li>Deploying</li>
+           </ul>
+        </p>       
+        </div>
+        <div class="col-lg-6 p-6 order-lg-4 my-auto text-center">
+            <br/>
+            <br/>
+            <br/>
+            <h2 class="display-5 mb-0" style="text-align:center"> Speak to our advisors about your business requirement </h2>
+            <br/>
+            <p class="lead mb-0" style="text-align: justify;font-weight:500;">Submit your contact details and we’ll be in touch shortly.</p>
+            
+        <br>
+            <form>
+              <div class="form-row">
+                <div class="col-12 col-md-9 mb-2 mb-md-0">
+                  <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
+                </div>
+                <div class="col-12 col-md-3">
+                  <button type="submit" class="btn btn-block btn-lg btn-primary">Submit</button>
+                </div>
+              </div>
+            </form> 
+          
+
+        </div>
+</section>
+
+
+
+
+        <!-- Footer -->
+<footer class="footer bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+                <ul class="list-inline mb-2">
+                    <li class="list-inline-item">
+                        <a href="AboutUs.php">About</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="ContactUs.php">Contact</a>
+                    </li>
+                    
+                </ul>
+                <p class="text-muted small mb-4 mb-lg-0">&copy; The Hive Office Suites 2022. All Rights Reserved.</p>
+            </div>
+            <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item mr-3">
+                        <a href="#">
+                            <i class="fab fa-facebook fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item mr-3">
+                        <a href="#">
+                            <i class="fab fa-twitter-square fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <i class="fab fa-instagram fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ HiveAdvantages.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Hive Office Suites </title>
+
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="img/Background.svg" /> <!--LOGO At the topp of the search bar -->
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/landing-page.css" rel="stylesheet">
+  
+  </head>
+  <body>
+      <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="mainNav">
+              <div class="container">
+                  <a class="navbar-brand" href="#page-top"><img src="img/Aushaan-logo.svg" alt="..." /></a><!--LOGO At the navigation bar -->
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                      Menu
+                      <i class="fas fa-bars ms-1"></i>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarResponsive">
+                      <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0" >
+                          <li class="nav-item"><a class="nav-link" href="LandingPage.php">HOME</a></li>
+                          <li class="nav-item"><a class="nav-link" href="Membership.php">MEMBERSHIP</a></li>
+                          <li class="nav-item"><a class="nav-link" href="CallCenter.php">CALL CENTER</a></li>
+                          <li class="nav-item"><a class="nav-link" href="ContactUs.php">CONTACT US</a></li> 
+                          <li class="nav-item"><a class="nav-link" href="AboutUs.php">ABOUT US</a></li>
+                      </ul>
+                  </div>
+              </div>
+    </nav>
+        
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</header>
+
+
+<div ></div>
+
+
+</div>
+
+<section class="showcase">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters">
+
+          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/office3.jpeg');"></div>
+          <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h3 style="color: #233a77;text-align: left;" class="display-5 mb-0" >Be at Home at Work</h3><br>
+              <p class="lead mb-0" style="text-align: justify;font-weight:400;">  The Hive is best experienced in person. Come in and take a look at what is offered.
+ <br>
+<br>
+<ul class="lead mb-0" style="text-align: justify;font-weight:400;">
+
+<li>Flexible Membership options  </li>
+<li>Flexible work spaces  </li>
+<li>24/7 Members Access </li>
+<li>Seamless high-speed WIFI</li>
+<li>Quiet Private office suites  </li>
+<li>Quality Networking Opportunities </li>
+<li>Ideal Locations in the Hub of Nairobi </li>
+<li>Dedicated Meeting Rooms </li>
+<li>On Site Secure Parking  </li>
+<li>Collaborative spaces </li>
+<li>Free coffee </li>
+<li>Guests Reception and services </li>
+<li>Dedicated lockers and drawers </li>
+<li>Telephone and mail handling services </li>
+<li>Dedicated Business address </li>
+<li>Access to world class events </li>
+<li>Free office cleaning services </li>
+<li>Fully Services Kitchenette </li>
+<li>Presentation facilities </li>
+<li>Printing and copying services </li>
+</ul> <br>
+<h4>The Hive is best experienced in person. Come in and take a look.</h4>
+</p> 
+        </div>
+        </div>
+        </div>
+        <br>
+
+<!-- Trial 
+<section>
+<div class="py-5" > 
+            <div class="container"style="text-align: centers;">
+              <h2 class="mb-5 text-center">Be at Home and at Work</h2>
+                <div class="row align-items-center">
+                    <p class="lead mb-0" style="text-align:center;font-weight:400; font-size:24px;">
+                    The Hive is best experienced in person. Come in and take a look. 
+                      </p>
+                </div>
+            </div>
+    </div>
+        <div class="row no-gutters">
+        <div class="col-lg-6 p-6 order-lg-4 my-auto text-center">
+            <br/>
+            <br/>
+            <p class="lead mb-0" style="text-align:center">
+        <ul class="lead mb-0" style="text-align: justify;font-weight:400;">
+<li >Flexible Membership options  </li>
+<li >Flexible work spaces  </li>
+<li>24/7 Members Access </li>
+<li>Seamless high-speed WIFI</li>
+<li>Quiet Private office suites  </li>
+<li>Quality Networking Opportunities </li>
+<li>Ideal Locations in the Hub of Nairobi </li>
+<li>Dedicated Meeting Rooms </li>
+<li>On Site Secure Parking  </li>
+<li>Collaborative spaces </li>
+
+</ul>
+        </p>       
+        </div>
+        <div class="col-lg-6 p-6 order-lg-4 my-auto text-center">
+            <br/>
+            <ul class="lead mb-0" style="text-align: justify;font-weight:400;">
+<li>Free coffee </li>
+<li>Guests Reception and services </li>
+<li>Dedicated lockers and drawers </li>
+<li>Telephone and mail handling services </li>
+<li>Dedicated Business address </li>
+<li>Access to world class events </li>
+<li>Free office cleaning services </li>
+<li>Fully Services Kitchenette </li>
+<li>Presentation facilities </li>
+<li>Printing and copying services </li>
+            </ul>          
+            </div>
+            </div>
+            </section>
+
+-->
+<!-- Call to Action -->
+<section class="call-to-action text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-9 mx-auto">
+            <h2 class="mb-4"> <br>Are you an entrepreneur, a freelancer, a consultant or a creative 
+            building a small business or running a start-up enterprise?  </h2>
+            </div>
+            <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                <div class="d-grid gap-2 col-6 mx-auto">
+                
+                    <a href='ContactUs.php'>
+                        <button class="btn btn-primary" type="button">Get in Touch</button>
+                    </a>
+                </div>
+                
+   
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Footer -->
+<footer class="footer bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+                <ul class="list-inline mb-2">
+                    <li class="list-inline-item">
+                        <a href="AboutUs.php">About</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="ContactUs.php">Contact</a>
+                    </li>
+                  
+                    </li>
+                </ul>
+                <p class="text-muted small mb-4 mb-lg-0">&copy; The Hive Office Suites 2022. All Rights Reserved.</p>
+            </div>
+            <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item mr-3">
+                        <a href="#">
+                            <i class="fab fa-facebook fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item mr-3">
+                        <a href="#">
+                            <i class="fab fa-twitter-square fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <i class="fab fa-instagram fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+  </body>
+  </html>
